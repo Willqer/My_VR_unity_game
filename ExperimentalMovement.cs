4f1c011092646e6c5797f2e1bf2f76ef78aa9057
Wojciech Kuliński <73992453+Willqer@users.noreply.github.com>
2022-06-09 16:31:11 +0200

    Add files via upload

---
+++ ExperimentalMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentalMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject AvatarBody;
+    [SerializeField] private GameObject AvatarCam;
+    [SerializeField] private CharacterController AvatarCharCont;
+    [SerializeField] private float speed = 1.0f;
+
+    private ControlsMap maping;
+    //==========================
+    private void OnEnable()
+    {
+        maping.Enable();
+    }
+    private void OnDisable()
+    {
+        maping.Disable();
+    }
+    //==========================
+    private void Awake()
+    {
+        maping = new ControlsMap();
+    }
+    void Update()
+    {
+
+        Vector2 Go = maping.Character.Movement.ReadValue<Vector2>();
+
+        float GoSideways = Input.GetAxis("axisY");
+        float GoFrontBack = Input.GetAxis("axisX");
+
+        Vector3 Direction = (AvatarCam.transform.right * (-1) * GoSideways) + (AvatarCam.transform.forward * (-1) * GoFrontBack);
+        AvatarCharCont.Move(Direction * speed * Time.deltaTime);
+
+        Vector3 NewDirection = (AvatarCam.transform.right * Go[0]) + (AvatarCam.transform.forward * Go[1]);
+        AvatarCharCont.Move(NewDirection * speed * Time.deltaTime);
+    }
+}
---
+++ TestAxis.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAxis : MonoBehaviour
+{
+    [SerializeField] private GameObject Cube1;
+    [SerializeField] private GameObject Cube2;
+    [SerializeField] private GameObject Cube3;
+    [SerializeField] private GameObject Cube4;
+    [SerializeField] private GameObject Cube5;
+    [SerializeField] private GameObject Cube6;
+    [SerializeField] private GameObject Cube7;
+    [SerializeField] private GameObject Cube8;
+    [SerializeField] private GameObject Cube9;
+    [SerializeField] private GameObject Cube10;
+    [SerializeField] private GameObject Cube11;
+    [SerializeField] private GameObject Cube12;
+    [SerializeField] private GameObject Cube13;
+    [SerializeField] private GameObject Cube14;
+    [SerializeField] private GameObject Cube15;
+    [SerializeField] private GameObject Cube16;
+    [SerializeField] private GameObject Cube17;
+    [SerializeField] private GameObject Cube18;
+    [SerializeField] private GameObject Cube19;
+    [SerializeField] private GameObject Cube20;
+    [SerializeField] private GameObject Cube21;
+    [SerializeField] private GameObject Cube22;
+    [SerializeField] private GameObject Cube23;
+    [SerializeField] private GameObject Cube24;
+    [SerializeField] private GameObject Cube25;
+    [SerializeField] private GameObject Cube26;
+    [SerializeField] private GameObject Cube27;
+    [SerializeField] private GameObject Cube28;
+
+    void Update()
+    {
+        float Axis1;
+        float Axis2;
+        float Axis3;
+        float Axis4;
+        float Axis5;
+        float Axis6;
+        float Axis7;
+        float Axis8;
+        float Axis9;
+        float Axis10;
+        float Axis11;
+        float Axis12;
+        float Axis13;
+        float Axis14;
+        float Axis15;
+        float Axis16;
+        float Axis17;
+        float Axis18;
+        float Axis19;
+        float Axis20;
+        float Axis21;
+        float Axis22;
+        float Axis23;
+        float Axis24;
+        float Axis25;
+        float Axis26;
+        float Axis27;
+        float Axis28;
+
+
+        Axis1 = Input.GetAxis("axisX");
+         Axis2 = Input.GetAxis("axisY");
+         Axis3 = Input.GetAxis("axis3");
+         Axis4 = Input.GetAxis("axis4");
+         Axis5 = Input.GetAxis("axis5");
+         Axis6 = Input.GetAxis("axis6");
+         Axis7 = Input.GetAxis("axis7");
+         Axis8 = Input.GetAxis("axis8");
+         Axis9 = Input.GetAxis("axis9");
+         Axis10 = Input.GetAxis("axis10");
+         Axis11 = Input.GetAxis("axis11");
+         Axis12 = Input.GetAxis("axis12");
+         Axis13 = Input.GetAxis("axis13");
+         Axis14 = Input.GetAxis("axis14");
+         Axis15 = Input.GetAxis("axis15");
+         Axis16 = Input.GetAxis("axis16");
+         Axis17 = Input.GetAxis("axis17");
+         Axis18 = Input.GetAxis("axis18");
+         Axis19 = Input.GetAxis("axis19");
+         Axis20 = Input.GetAxis("axis20");
+         Axis21 = Input.GetAxis("axis21");
+         Axis22 = Input.GetAxis("axis22");
+         Axis23 = Input.GetAxis("axis23");
+         Axis24 = Input.GetAxis("axis24");
+         Axis25 = Input.GetAxis("axis25");
+         Axis26 = Input.GetAxis("axis26");
+         Axis27 = Input.GetAxis("axis27");
+         Axis28 = Input.GetAxis("axis28");
+
+        if (Axis1  == -1f || Axis1  == 1f)
+        {
+            Cube1.SetActive(true);
+        }
+        if (Axis2  == -1f || Axis2  == 1f)
+        {
+            Cube2.SetActive(true);
+        }
+        if (Axis3  == -1f || Axis3  == 1f)
+        {
+            Cube3.SetActive(true);
+        }
+        if (Axis4  == -1f || Axis4  == 1f)
+        {
+            Cube4.SetActive(true);
+        }
+        if (Axis5  == -1f || Axis5  == 1f)
+        {
+            Cube5.SetActive(true);
+        }
+        if (Axis6  == -1f || Axis6  == 1f)
+        {
+            Cube6.SetActive(true);
+        }
+        if (Axis7  == -1f || Axis7  == 1f)
+        {
+            Cube7.SetActive(true);
+        }
+        if (Axis8  == -1f || Axis8  == 1f)
+        {
+            Cube8.SetActive(true);
+        }
+        if (Axis9  == -1f || Axis9  == 1f)
+        {
+            Cube9.SetActive(true);
+        }
+        if (Axis10 == -1f || Axis10 == 1f)
+        {
+            Cube10.SetActive(true);
+        }
+        if (Axis11 == -1f || Axis11 == 1f)
+        {
+            Cube11.SetActive(true);
+        }
+        if (Axis12 == -1f || Axis12 == 1f)
+        {
+            Cube12.SetActive(true);
+        }
+        if (Axis13 == -1f || Axis13 == 1f)
+        {
+            Cube13.SetActive(true);
+        }
+        if (Axis14 == -1f || Axis14 == 1f)
+        {
+            Cube14.SetActive(true);
+        }
+        if (Axis15 == -1f || Axis15 == 1f)
+        {
+            Cube15.SetActive(true);
+        }
+        if (Axis16 == -1f || Axis16 == 1f)
+        {
+            Cube16.SetActive(true);
+        }
+        if (Axis17 == -1f || Axis17 == 1f)
+        {
+            Cube17.SetActive(true);
+        }
+        if (Axis18 == -1f || Axis18 == 1f)
+        {
+            Cube18.SetActive(true);
+        }
+        if (Axis19 == -1f || Axis19 == 1f)
+        {
+            Cube19.SetActive(true);
+        }
+        if (Axis20 == -1f || Axis20 == 1f)
+        {
+            Cube20.SetActive(true);
+        }
+        if (Axis21 == -1f || Axis21 == 1f)
+        {
+            Cube21.SetActive(true);
+        }
+        if (Axis22 == -1f || Axis22 == 1f)
+        {
+            Cube22.SetActive(true);
+        }
+        if (Axis23 == -1f || Axis23 == 1f)
+        {
+            Cube23.SetActive(true);
+        }
+        if (Axis24 == -1f || Axis24 == 1f)
+        {
+            Cube24.SetActive(true);
+        }
+        if (Axis25 == -1f || Axis25 == 1f)
+        {
+            Cube25.SetActive(true);
+        }
+        if (Axis26 == -1f || Axis26 == 1f)
+        {
+            Cube26.SetActive(true);
+        }
+        if (Axis27 == -1f || Axis27 == 1f)
+        {
+            Cube27.SetActive(true);
+        }
+        if (Axis28 == -1f || Axis28 == 1f)
+        {
+            Cube28.SetActive(true);
+        }
+    }
+}
